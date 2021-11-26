b6f3c136ef6dea8b82960623166686603e7587b6
Fabio_Martinez <rok5150@gmail.com>
2021-11-26 10:17:33 -0300

    exercicios e exemplos

---
+++ Swift/Exemplos/main.swift
@@ -0,0 +1,73 @@
+
+
+
+//_ _ _ _ _ _ //
+
+//Console
+
+// var firstName = "Mickey"
+// var secondName = "Mouse"
+
+// //Concatenação
+// print(firstName + " " + secondName)
+
+//Interpolação
+// print("Eu sou o \(firstName) \(secondName), miska, muska, Mickey Mouse!!")
+
+// var number1 = 10
+// var number2 = 20
+
+// print("O meu primeiro número é: \(number1). O meu segundo é:  \(number2). Total é \(number1 + number2)")
+
+
+
+// _ _ _ _ _ //
+
+
+//Int
+
+// var age = 25
+
+// age = 25
+// print(age)
+
+// _ _ _ _ _ //
+
+//strings
+
+
+//var name = "Fábio Martinez"
+
+
+// _ _ _ _ _ //
+
+//Comentários
+
+//This is a comment
+
+/*This is also a comment...
+
+...and it ends here
+*/
+
+
+// _ _ _ _ _ _ //
+
+//Constantes
+
+// let name = "Fábio"
+// let age = 100
+
+// print("Meu nome é \(name), e eu tenho \(age) anos")
+
+
+// _ _ _ _ _ _ //
+
+
+
+//Variáveis
+
+// var name = "Fábio"
+// var age = 100
+// age = 50
+// print(age)
---
+++ Swift/Exercicios/1/main.swift
@@ -0,0 +1,7 @@
+var city = "São Carlos"
+
+let bornDate = 1987
+
+print("Eu nasci na cidade de \(city), no ano de \(bornDate)")
+
+print("Eu nasci na cidade de " + city + ", no ano de " + String(bornDate) + ".")
