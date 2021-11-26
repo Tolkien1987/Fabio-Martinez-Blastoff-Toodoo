ce6d479afc951c5b742263451ebe7597aedae777
Fabio_Martinez <rok5150@gmail.com>
2021-11-26 11:09:51 -0300

    exercicios e exemplos2

---
+++ Swift/Exercicios/2/main.swift
@@ -0,0 +1,13 @@
+//Criar Variáveis implicitas e explicitas
+
+var dadName = "Ricardo"
+var dadAge = 58
+var dadHeight = 1.55
+var dadBald = true
+
+var momName : String = "Bety"
+var momAge : Int = 60
+var momHeight : Double = 1.65645
+var momWeight : Float = 58.4
+var momPretty : Boll = true
+
