214b9f96817e1b55dabc372f5d62b733cdc96ffd
Fabio_Martinez <rok5150@gmail.com>
2021-11-29 11:50:39 -0300

    exercicio 5

---
+++ Swift/Exercicios/5/main.swift
@@ -0,0 +1,15 @@
+//Exercicio
+
+//Crie uma condicional if que contenha dentro de si outro codigo condicional if/Else e que imprima os dados no console
+
+var myName = "Fabio"
+var myComputer : Bool = true
+var time = 18
+
+if myComputer == true && time >= 18 {
+  print("\(myName), you can turn on your computer right now")
+} else {
+  print("\(myName), you cant turn on your computer before 18 o clock")
+}
+
+
