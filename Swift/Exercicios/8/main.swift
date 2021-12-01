be9c063a54d166fab94d20abd85dd130d5f00f37
Fabio_Martinez <rok5150@gmail.com>
2021-12-01 15:02:14 -0300

    exercicio 8

---
+++ Swift/Exercicios/8/main.swift
@@ -0,0 +1,32 @@
+//Exercicio
+
+//1 Crie um for loop que imprima no console 50 vezes a string "Programar e demais!"
+//2 Crie um loop que multiplique uma variavel de tipo int por ela 5 vezes
+//3 Criei um array de cores e um loop que imprima cada dor dentro daquela array
+
+
+//1
+
+for _ in 1...50 {
+  print ("Programar é demais!\n")
+}
+
+
+//2
+
+for n1 in 1...5 {
+  print("Valor da multiplicação é \(n1*n1)\n")
+}
+
+
+//3
+
+var colours : [String] = ["Black", "Rose", "Ambar", "Green", "Blue", "White"]
+
+for names in colours {
+  print(names)
+}
+
+
+
+
