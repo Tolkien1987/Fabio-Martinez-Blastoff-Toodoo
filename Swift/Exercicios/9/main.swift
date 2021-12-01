3d769d58ff57bb51f8dfcfb4c6b65a8040fa4967
Fabio_Martinez <rok5150@gmail.com>
2021-12-01 15:51:11 -0300

    exercicio 9

---
+++ Swift/Exercicios/9/main.swift
@@ -0,0 +1,14 @@
+//Exercicio while loops
+
+//1 Crie uma variável que seja menor do que 10 e multiplique-a por 5 até que ela seja menor ou igual a 5000
+
+
+//1
+var myScore = 1
+
+while myScore <= 5000 {
+  
+  print(myScore)
+  myScore *= 5
+}
+
